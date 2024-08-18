fb8e4f65983aa22890ffebbbda1eec62300291ba
Ghost <ghost@unknown.com>
2024-08-18 12:31:43 +0200

    Kernel size optimization

--- build.sh
+++ build.sh
@@ -5,7 +5,8 @@ set -e
 if [ "$1" == "build" ]; then
     nasm stage1.asm -o stage1.bin
     nasm stage2.asm -o stage2.bin
-    clang src/*.c kernel.c -o kernel.bin -nostdlib -Weverything -Wno-declaration-after-statement -Wno-unsafe-buffer-usage
+    clang src/*.c kernel.c -o kernel.bin -nostdlib -nodefaultlibs -fno-exceptions -fno-asynchronous-unwind-tables -Wl,--build-id=none -Wl,--no-dynamic-linker -Wl,-z,norelro -Weverything -Wno-declaration-after-statement -Wno-unsafe-buffer-usage
+    objcopy --remove-section=.gnu.hash --remove-section=.dynsym --remove-section=.dynstr --remove-section=.eh_frame --remove-section=.dynamic --remove-section=.comment kernel.bin
     strip kernel.bin
     if [ $(stat -c %s kernel.bin) -gt 16384 ]; then
         echo "Kernel code exceeds 16384 bytes." >&2
