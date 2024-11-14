1bdcdb663c890ae5521389da570df1acc29c380f
ghost <michal.miladowski@gmail.com>
2024-11-14 15:43:37 +0100

    HAL

--- hal.c
+++ hal.c
@@ -1,5 +1,23 @@
 #include "hal.h"
 
+uint8_t PortInByte(uint16_t port) {
+    uint8_t v;
+    __asm("inb %1, %0" : "=a" (v) : "d" (port));
+    return v;
+}
+
+uint16_t PortInWord(uint16_t port) {
+    uint16_t v;
+    __asm("inw %1, %0" : "=a" (v) : "d" (port));
+    return v;
+}
+
+uint32_t PortInDword(uint16_t port) {
+    uint32_t v;
+    __asm("inl %1, %0" : "=a" (v) : "d" (port));
+    return v;
+}
+
 void PortOutByte(uint16_t port, uint8_t v) {
     __asm("outb %0, %1" : : "a" (v), "d" (port));
 }
--- hal.h
+++ hal.h
@@ -2,6 +2,10 @@
 
 #include "types.h"
 
+uint8_t PortInByte(uint16_t port);
+uint16_t PortInWord(uint16_t port);
+uint32_t PortInDword(uint16_t port);
+
 void PortOutByte(uint16_t port, uint8_t v);
 void PortOutWord(uint16_t port, uint16_t v);
 void PortOutDword(uint16_t port, uint32_t v);
