eac36f23a37ebb73c9693b067e7c9fa250edd9f4
ghost <ghost@unknown.com>
2025-12-17 13:13:15 +0100

    Tests

--- kernel64/src/main.rs
+++ kernel64/src/main.rs
@@ -1,17 +1,87 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 
+mod uart;
 mod vga_text_buffer;
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
+    #[cfg(test)]
+    test_main();
+
     loop {}
 }
 
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
+
     loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+
+    exit_qemu(QemuExitCode::Failure);
+
+    loop {}
+}
+
+// Exit codes shound not overlap with QEMU's own exit codes
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+enum QemuExitCode {
+    Success = 0x10,
+    Failure = 0x11,
+}
+
+fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    let mut port = Port::new(0xf4);
+    unsafe {
+        port.write(exit_code as u32);
+    }
+}
+
+// &[&dyn Fn()]
+#[cfg(test)]
+fn test_runner(tests: &[&dyn TestCase]) {
+    serial_println!("\nRunning {} tests", tests.len());
+    for test in tests {
+        test.run();
+    }
+
+    exit_qemu(QemuExitCode::Success);
+}
+
+trait TestCase {
+    fn run(&self) -> ();
+}
+
+impl<T> TestCase for T
+where
+    T: Fn(),
+{
+    fn run(&self) {
+        serial_print!("{}... ", core::any::type_name::<T>());
+
+        self();
+
+        serial_println!("[ok]");
+    }
+}
+
+#[test_case]
+fn trivial_assertion() {
+    assert_eq!(1, 1);
 }
